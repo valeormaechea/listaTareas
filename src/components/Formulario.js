3a3d1021008f23239174033101a4a22af16f61e3
M. Valentina Ormaechea <ormaecheavale@gmail.com>
2022-08-04 00:50:35 -0300

    agregamos el state arregloTareas y lo enviamos por props al componente listaTareas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -5,23 +5,46 @@ import ListaTareas from "./ListaTareas";
 
 const Formulario = () => {
   // Logica
+
   // Crear un state
   const [arregloTareas, setArregloTareas] = useState([]);
   const [tarea, setTarea] = useState("");
 
+  // const actualizarTarea = (e) => {
+  //   // Obtener el value del input
+  //   console.log(e.target.value);
+  //   // Actualizar el state
+  //   setTarea(e.target.value.trim());
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("prueba desde submit");
+    // Actualizar la lista de tareas
+    console.log([...arregloTareas, tarea]);
+    setArregloTareas([...arregloTareas, tarea]);
+    setTarea("");
+  };
+
   // maquetado y algo de logica
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value.trim())}
+            // Limpio el value junto con como limpio el state
+            value={tarea}
+          />
           <Button variant="primary" type="submit" className="ms-3">
             Enviar
           </Button>
         </Form.Group>
       </Form>
       {/* Invoco a la lista de tareas */}
-      <ListaTareas></ListaTareas>
+      <ListaTareas arregloTareas={arregloTareas}></ListaTareas>
     </div>
   );
 };
